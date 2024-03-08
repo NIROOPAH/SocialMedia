237be2b81481a7d968be7e2954f19682f1a21cc1
NIROOPAH <nagadarshan16121997@gmail.com>
2024-03-08 22:15:31 +0530

    Crud Operations with Authentication

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,10 +1,13 @@
 import UserModel from '../Models/userModel.js';
+import bcrypt from 'bcrypt'
 
 // Registration of users
 export const registerUser = async (req, res) => {
     const { username, password, firstname, lastname } = req.body;
+    const salt=await bcrypt.genSalt(10)
+    const hashpassword=await bcrypt.hash(password,salt)
 
-    const newUser = new UserModel({ username, password, firstname, lastname });
+    const newUser = new UserModel({ username,password:hashpassword, firstname, lastname });
 
     try {
         await newUser.save();
@@ -13,4 +16,27 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+//login user
+
+export const loginUser= async (req,res)=>{
+    const {username,password}=req.body
+
+    try {
+        const user=await UserModel.findOne({username:username})
+
+        if(user)
+        {
+            const validity = await bcrypt.compare(password,user.password)
+
+            validity?res.status(200).json(user):res.status(400).json("Wrong Password")
+        }
+        else
+        {
+            res.status(404).json("User Not Found")
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+}
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import AuthRoute from "./Routes/AuthRoute.js";
+import UserRoute from "./Routes/UserRoute.js";
 
 const app=express();
 
@@ -11,7 +12,7 @@ app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_DB)
   .then(() => {
     app.listen(process.env.PORT, () => console.log(`Listening at PORT ${process.env.PORT}`));
   })
@@ -19,5 +20,7 @@ mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopolo
     console.log(error);
   });
 
-//routes
+
+//routes 
 app.use('/auth',AuthRoute)
+app.use('/user',UserRoute)
